264f6ec7b03dbf709c4020f11b173c7bb6baccdd
lvliangjun <zh1@vip.qq.com>
2024-08-30 13:50:40 +0800

    AnonymousField

--- structUtil/convert.go
+++ structUtil/convert.go
@@ -3,7 +3,7 @@ package structUtil
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/go-tron/types-util/stringUtil"
+	"github.com/go-tron/types/stringUtil"
 	"github.com/thoas/go-funk"
 	"reflect"
 	"sort"
@@ -14,6 +14,7 @@ type ToSortStringOption func(*ToSortStringConfig)
 type ToSortStringConfig struct {
 	Exclude        []string
 	IgnoreEmptyStr bool
+	AnonymousField bool
 }
 
 func ToSortStringWithExclude(val ...string) ToSortStringOption {
@@ -26,6 +27,13 @@ func ToSortStringWithIgnoreEmptyStr() ToSortStringOption {
 		opts.IgnoreEmptyStr = true
 	}
 }
+
+func ToSortStringWithAnonymousField() ToSortStringOption {
+	return func(opts *ToSortStringConfig) {
+		opts.AnonymousField = true
+	}
+}
+
 func ToSortString(obj interface{}, opts ...ToSortStringOption) string {
 	if obj == nil {
 		return ""
@@ -46,14 +54,7 @@ func ToSortString(obj interface{}, opts ...ToSortStringOption) string {
 	}
 	t := v.Type()
 
-	var keys []string
-	for k := 0; k < t.NumField(); k++ {
-		if len(c.Exclude) > 0 && funk.ContainsString(c.Exclude, t.Field(k).Name) {
-			continue
-		}
-		keys = append(keys, t.Field(k).Name)
-	}
-	sort.Strings(keys)
+	keys := SortKeys(t, c)
 
 	str := ""
 	for _, key := range keys {
@@ -89,72 +90,21 @@ func ToSortString(obj interface{}, opts ...ToSortStringOption) string {
 	return str
 }
 
-// ToSortStringIncludeAnonymous 处理匿名属性的排序方法
-// type A struct{Age int}  type B struct {A Name string} ,var aa = B{Name:'xx',{Age:18}}
-// 预期得到字符串 age=18&name=xx, ToSortString 得到 A={18}&name=xx
-func ToSortStringIncludeAnonymous(obj interface{}, exclude ...string) string {
-	if obj == nil {
-		return ""
-	}
-	v := reflect.ValueOf(obj)
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			return ""
-		}
-		v = v.Elem()
-	}
-	t := v.Type()
-
-	keys := SortKeys(t, exclude...)
-
-	str := ""
-	for _, key := range keys {
-		if str != "" {
-			str += "&"
-		}
-		field := v.FieldByName(key)
-		name := stringUtil.FirstCharToLower(key)
-
-		//fmt.Printf("%s:%s:%v\n", name, field.Type(), field.Interface())
-		var value string
-		if field.IsZero() {
-			value = ""
-		} else {
-			if field.Kind() == reflect.Ptr {
-				field = field.Elem()
-			}
-
-			if field.Kind() == reflect.Map || field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
-				byteVal, err := json.Marshal(field.Interface())
-				if err == nil {
-					value = string(byteVal)
-				} else {
-					value = fmt.Sprint(field.Interface())
-				}
-			} else {
-				value = fmt.Sprint(field.Interface())
-			}
-		}
-		str += name + "=" + value
-	}
-	return str
-}
-
-func SortKeys(t reflect.Type, exclude ...string) []string {
+func SortKeys(t reflect.Type, c *ToSortStringConfig) []string {
 	var keys []string
 	for k := 0; k < t.NumField(); k++ {
-		if len(exclude) > 0 && funk.ContainsString(exclude, t.Field(k).Name) {
+		if len(c.Exclude) > 0 && funk.ContainsString(c.Exclude, t.Field(k).Name) {
 			continue
 		}
 		if t.Field(k).Anonymous {
-			res := SortKeys(t.Field(k).Type, exclude...)
-			keys = append(keys, res...)
+			if c.AnonymousField {
+				res := SortKeys(t.Field(k).Type, c)
+				keys = append(keys, res...)
+			}
 		} else {
 			keys = append(keys, t.Field(k).Name)
 		}
 	}
-
 	sort.Strings(keys)
-
 	return keys
 }
